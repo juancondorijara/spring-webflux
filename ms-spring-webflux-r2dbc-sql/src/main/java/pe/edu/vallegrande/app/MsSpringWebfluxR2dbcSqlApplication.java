87b6b2c21aab4db1f485ae4c890dbe4bb65d0e78
Juan Gabriel Condori Jara <juan.condori.jara@vallegrande.edu.pe>
2024-08-26 15:05:39 -0500

    SQL - NOSQL

---
+++ ms-spring-webflux-r2dbc-sql/src/main/java/pe/edu/vallegrande/app/MsSpringWebfluxR2dbcSqlApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.vallegrande.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsSpringWebfluxR2dbcSqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsSpringWebfluxR2dbcSqlApplication.class, args);
+	}
+
+}
---
+++ ms-spring-webflux-r2dbc-sql/src/test/java/pe/edu/vallegrande/app/MsSpringWebfluxR2dbcSqlApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.vallegrande.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsSpringWebfluxR2dbcSqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
